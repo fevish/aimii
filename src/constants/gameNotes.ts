4a531f8b926b8069190c324d4775b8dbe891a658
fevish <tuomas@letsembark.ca>
2026-03-28 11:14:00 -0600

    re-write arc raiders note content, adds marathon note

--- src/constants/gameNotes.ts
+++ src/constants/gameNotes.ts
@@ -1,7 +1,12 @@
 export const gameNotesByName: Record<string, string> = {
   'ARC Raiders': `
     <p>
-      Recommended: Match <b>Horizontal</b> and <b>Vertical</b> sliders to the same sensitivity, and disable <b>Mouse Smoothing</b>.
+      Recommended: Set [<b>Horizontal</b> and <b>Vertical</b> sliders] to the same sensitivity, and turn [<b>Mouse Smoothing</b>: OFF]
+    </p>
+  `,
+  'Marathon': `
+    <p>
+      Recommended: Turn [<b>Aim Smoothing: Off</b>]
     </p>
   `,
 };
